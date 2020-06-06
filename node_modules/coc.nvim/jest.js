95442bf1b71139df522c78f7951fd807211b69b2
Hayden Young <hayden@itshayden.dev>
2020-06-06 12:42:45 +0100

    removed node_modules

--- node_modules/coc.nvim/jest.js
+++
@@ -1,9 +0,0 @@
-
-process.on('uncaughtException', function(err) {
-  let msg = 'Uncaught exception: ' + err.stack
-  console.error(msg)
-})
-
-module.exports = async () => {
-  process.env.NODE_ENV = 'test'
-}
